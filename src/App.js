e69cedc60feaeba8c3c7845b75a6e82847a5731c
GT <FTowerT@outlook.com>
2021-07-11 13:40:47 +0300

    Added styles for an app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import logo from './logo.svg';
+//vedor
+import 'antd/dist/antd.css'; //Styles for all antd components, if you remove this - antd will not work
 
 //proj
 import DuckGeteratorPage from './DuckGeneratorPage';
+import logo from './logo.svg';
 
 //own
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="title">
-          Boilerplate reducer
-        </div>
-      </header>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<div className="title">
+					Boilerplate reducer
+				</div>
+			</header>
 
-      <div>
-        <DuckGeteratorPage />
-      </div>
-    </div>
-  );
+			<div>
+				<DuckGeteratorPage />
+			</div>
+		</div>
+	);
 }
 
 export default App;
